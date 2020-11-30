5944821e54273d5723efc24c0fc7155edca7f86c
gonagutor <gonagutor@gmail.com>
2020-11-30 16:33:41 +0100

    :rocket: Updated error messages to show an error code

--- utilities/errorMessages.js
+++ utilities/errorMessages.js
@@ -1,42 +1,56 @@
 // errorMessages.js
 
 exports.databaseError = function(res, err){
-  res.json({
+  res.status(500).json({
     status: "error",
     data: err,
   });
 };
 
 exports.invalidToken = function (res) {
-  res.json({
+  res.status(401).json({
     status: "error",
     data: "Your token is invalid",
   });
 }
 
+exports.missingToken = function (res) {
+  res.status(401).json({
+    status: "error",
+    data: "Your token is not set",
+  });
+}
+
+exports.wrongPassword = function (res) {
+  res.status(401).json({
+    status: "error",
+    data: "Your password is incorrect",
+  });
+}
+
 exports.characterDoesNotExist = function (res) {
-  res.json({
+  res.status(404).json({
     status: "error",
     data: "That character does not exist"
   });
 }
 
 exports.wrongOwnership = function (res) {
-  res.json({
+  res.status(401).json({
     status: "error",
     data: "This is not your character",
   });
 }
 
 exports.wrongAuthority = function (res) {
-  res.json({
+  res.status(401).json({
     status: "error",
     data: "You are not allowed to do that",
   });
 }
 
 exports.wrongRequest = function (res) {
-  res.json({
+  res.status(400).json({
     status: "error",
     data: "Wrong request",
   });
