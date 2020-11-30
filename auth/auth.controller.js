fe3553614a3e5ffb4bc3af6f03630ac01e6c9120
gonagutor <gonagutor@gmail.com>
2020-11-30 11:38:53 +0100

    :lock: Reimplemented token system and added an auth function

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -4,31 +4,36 @@ var User = require("./auth.model");
 var errorMessages = require("../utilities/errorMessages");
 var dotenv = require("dotenv");
 var crypto = require("crypto");
+var jwt = require("jsonwebtoken");
 dotenv.config();
+
+exports.auth = function (req, res, callback) {
+  const token = req.header('token');
+  if (!token) return res.status(401).json({
+    status: "error",
+    data: "Your token is not set",
+  });
+  try {
+    const verifiedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verifiedToken;
+  } catch (err) {
+    errorMessages.invalidToken(res);
+  }
+}
 
 exports.register = function (req, res) {
   var newUser = new User();
-  if (
-    req.body.username == null &&
-    req.body.password == null &&
-    req.password.email == null
-  ) {
-    res.json({
-      status: "error",
-      dat: "Wrong request",
-    });
-    return;
-  }
+  if (!req.body.username && !req.body.password && !req.password.email)
+    return errorMessages.wrongRequest(res);
   newUser.username = req.body.username;
   newUser.characters = "/user/characters/" + newUser.username;
-  newUser.password = crypto
-    .createHash("sha512")
+  newUser.password = crypto.createHash("sha512")
     .update(process.env.SALT + req.body.password)
     .digest("hex");
-  newUser.token = "";
   newUser.email = req.body.email;
   newUser.authorization = 1;
   newUser.verified = 0;
+  newUser.creationDate = Date.now();
   newUser.save(function (err, user) {
     if (err)
       errorMessages.databaseError(res, err);
@@ -40,36 +45,22 @@ exports.register = function (req, res) {
 };
 
 exports.getToken = function (req, res) {
-  if (req.body.password == null) {
-    errorMessages.wrongRequest(res);
-    return;
-  }
+  if (!req.body.password || !req.body.user)
+    return errorMessages.wrongRequest(res);
   User.findOne({ username: req.params.user }, function (err, user) {
-    if (err)
-      errorMessages.databaseError(res, err);
-    else if ( // Verify password
-      req.body.password != null &&
-      crypto
-        .createHash("sha512")
-        .update(process.env.SALT + req.body.password)
-        .digest("hex") == user.password
-    ) {
-      if (user.token == null || user.token == "") { // If the token does not exist then create a new one for the user
-        user.token = crypto
-          .createHash("sha512")
-          .update(user.username + user.password)
-          .digest("hex");
-        user.save(function (err) {
-          if (err)
-            errorMessages.databaseError(res, err);
-        });
-      }
-      res.json({
-        status: "success",
-        data: user.token,
-      });
-    } else {
-      errorMessages.wrongAuthority(res);
-    }
+    const suposedPassword = crypto
+      .createHash("sha512")
+      .update(process.env.SALT + req.body.password)
+      .digest("hex");
+    if (err) return errorMessages.databaseError(res, err);
+    if (suposedPassword != user.password) errorMessages.wrongAuthority(res); // Verify password
+    const signedToken = jwt.sign({
+      username: username,
+      authorization: user.authorization
+    }, process.env.TOKEN_SECRET);
+    res.header('token', signedToken).json({
+      status: "success",
+      data: verifiedToken,
+    });
   });
 };
