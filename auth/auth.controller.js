143419515c9bbcde55f11a9db7251437521dc66a
gonagutor <gonagutor@gmail.com>
2020-12-06 21:31:11 +0100

    :lock: Added check for duplicate username and email

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -5,6 +5,7 @@ var errorMessages = require("../utilities/errorMessages");
 var dotenv = require("dotenv");
 var crypto = require("crypto");
 var jwt = require("jsonwebtoken");
+const { checkDuplicateByIndex } = require("../utilities/checkDuplicate");
 dotenv.config();
 
 /**
@@ -17,11 +18,12 @@ dotenv.config();
  */
 
 exports.auth = function (req, res, minLevelAuth, callback) {
-  const token = req.header('token');
+  const token = req.header("token");
   if (!token) return errorMessages.missingToken(res);
   try {
     const verifiedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-    if (verifiedToken.authorization < minLevelAuth) return errorMessages.wrongAuthority(res);
+    if (verifiedToken.authorization < minLevelAuth)
+      return errorMessages.wrongAuthority(res);
     User.findOne({ username: verifiedToken.username }, function (err, user) {
       if (err) return errorMessages.databaseError(res, err);
       if (!user) return errorMessages.invalidToken(res);
@@ -30,7 +32,7 @@ exports.auth = function (req, res, minLevelAuth, callback) {
   } catch (err) {
     errorMessages.invalidToken(res);
   }
-}
+};
 
 /**
  * Registers a new user
@@ -40,24 +42,28 @@ exports.auth = function (req, res, minLevelAuth, callback) {
  */
 
 exports.register = function (req, res) {
-  var newUser = new User();
   if (!req.body.username && !req.body.password && !req.password.email)
     return errorMessages.wrongRequest(res);
-  newUser.username = req.body.username;
-  newUser.characters = "/user/characters/" + newUser.username;
-  newUser.password = crypto.createHash("sha512")
-    .update(process.env.SALT + req.body.password)
-    .digest("hex");
-  newUser.email = req.body.email;
-  newUser.authorization = 1;
-  newUser.verified = 0;
-  newUser.creationDate = Date.now();
-  newUser.save(function (err, user) {
-    if (err)
-      errorMessages.databaseError(res, err);
-    res.json({
-      status: "success",
-      data: "New user created",
+  checkDuplicateByIndex(req, res, "username", User, function (req, res) {
+    checkDuplicateByIndex(req, res, "email", User, function (req, res) {
+      var newUser = new User();
+      newUser.username = req.body.username;
+      newUser.characters = "/user/characters/" + newUser.username;
+      newUser.password = crypto
+        .createHash("sha512")
+        .update(process.env.SALT + req.body.password)
+        .digest("hex");
+      newUser.email = req.body.email;
+      newUser.authorization = 1;
+      newUser.verified = 0;
+      newUser.creationDate = Date.now();
+      newUser.save(function (err, user) {
+        if (err) errorMessages.databaseError(res, err);
+        res.json({
+          status: "success",
+          data: "New user created",
+        });
+      });
     });
   });
 };
@@ -70,7 +76,8 @@ exports.register = function (req, res) {
  */
 
 exports.getToken = function (req, res) {
-  if (!req.body.password || !req.body.user) return errorMessages.wrongRequest(res);
+  if (!req.body.password || !req.body.user)
+    return errorMessages.wrongRequest(res);
   User.findOne({ username: req.body.user }, function (err, user) {
     const suposedPassword = crypto
       .createHash("sha512")
@@ -78,11 +85,14 @@ exports.getToken = function (req, res) {
       .digest("hex");
     if (err) return errorMessages.databaseError(res, err);
     if (suposedPassword != user.password) errorMessages.wrongPassword(res); // Verify password
-    const signedToken = jwt.sign({
-      username: user.username,
-      authorization: user.authorization
-    }, process.env.TOKEN_SECRET);
-    res.header('token', signedToken).json({
+    const signedToken = jwt.sign(
+      {
+        username: user.username,
+        authorization: user.authorization,
+      },
+      process.env.TOKEN_SECRET
+    );
+    res.header("token", signedToken).json({
       status: "success",
       data: signedToken,
     });
