6251aceb0c44706123de5c0096df538d0ba246ae
gonagutor <gonagutor@gmail.com>
2020-12-07 14:17:43 +0100

    :rocket: Class model complete

---
+++ classLevels/class.levels.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const classLevelsSchema = mongoose.Schema({
+  index: String,
+  level: Number,
+  ownerClass: String,
+  abilityScoreBonuses: Number,
+  profBonus: Number,
+  featureChoices: [
+    {
+      index: String,
+      name: String,
+      url: String,
+    },
+  ],
+  features: [
+    {
+      index: String,
+      name: String,
+      url: String,
+    },
+  ],
+  classSpecific: {
+    rageCount: Number,
+    rageDamageBonus: Number,
+    brutalCriticalDice: Number,
+  },
+  class: {
+    index: String,
+    name: String,
+    url: String,
+  },
+});
+
+var ClassLevels = (module.exports = mongoose.model(
+  "classLevels",
+  classLevelsSchema
+));
+
+module.exports.get = function (callback, limit) {
+  ClassLevels.find(callback).limit(limit);
+};
